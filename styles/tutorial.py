a14b4c8a0ddb7fd71b5c14eeaf77d201fd8a124c
Kevin Eales <kev0.0.0.255@gmail.com>
2021-09-15 09:57:47 -0400

    Framing out the config parser.

--- styles/tutorial.py
+++ styles/tutorial.py
@@ -7,10 +7,55 @@ This program is experimental and proprietary, redistribution is prohibited.
 Please see the license file for more details.
 ------------------------------------------------------------------------------------------------------------------------
 
-This is our tutorial style, used for teaching-intra-testing.
+This is our tutorial style,
+
+Its important to note that some of these values are in markup and will be translated when the style is read into
+    the logic. For example when a field valur starts with a doller sign '$_screen_width:100%' it will be evaluated
+    based on the statement following the colon,
+in this instance we will get a calculated value of 100% of the screen width. The underscore tells us that the value
+    we are calculating against is outside of the style, alternatively we are able to calculate against other fields in
+    the style as such: '$price_canvas_height:50%'. This will give us a calculated value of half of whatever the
+    field 'price_canvas_height' contains.
+
+Be aware that many of the naming conventions for the various parameters here are defined by third party logic and
+    a result of this there is no standard naming convention. This will be corrected in a future build.
 """
 
-style = {
-    'style_name': 'tutorial',  # Name style for organization.
+style_base = {
+    'main': {
+        'style_name': 'tutorial',  # Name style for organization.
+        # Configure global settings in relation to streen size and what have you.
+        'geometry': "$_screen_size:",  # This is our screen size.
+        'price_canvas_width:': '$_screen_width:100%',  # The size of the graphiend canvas.
+        'price_canvas_height': 350,
+        # This scales the candlestick matrix in order to make room for the other widgets.
+        'price_matrix_offsets': (50, 50, 35, 150),  # left, right, top, bottom.
+    },
     # From this point we wil divide this style into sections including the relational configuration respectively.
+    'candlesticks': {
+        # The bar_width will alter the X coordinates of all the price related widgets.
+        'bar_width': 8,  # The width of one candlestick in pixels.
+        'geometry': '$_price_matrix:',
+        'height': '$_screen_height:50%',
+        'color_1': 'green',
+        'color_2': 'red',
+        'alpha': 0.75,
+        'hollow': ['red']  # This can hole one none or both of the colors.
+    },
+    'smoothi_bottom': {
+        'geometry': '$_price_matrix:',
+        'height': 75,
+        'fill': 'aqua',
+        'grad': ('deepskyblue', 'black', 'v'),
+        'graph_type': 'volume',
+        'tb': 'b',
+        'outline': 'white',
+        'smooth': 5,
+        'lineinterpol': 3,
+        'offset': 300,
+        'padding': (47, 47, 0, 0),  # left, right, top, bottom.
+        'alpha': 0.2,
+        'alphamask': True,
+        'aa': (10, 0)
+    }
 }
--- utils.py
+++ utils.py
@@ -14,6 +14,63 @@ import configparser
 WORKING_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 
 
+def get_math():
+    """
+    This imports eaither numpy or cupy depending on what's around.
+    :return: math module
+    """
+    try:
+        # noinspection PyPackageRequirements
+        import cupy as mm  # This is for brutal math performance in the event we are running on a jetson.
+    except ImportError:
+        import numpy as mm  # This is the standard shit for everything else.
+    return mm
+
+
+np = get_math()
+
+
+def nparray(lst: [list, np.array]) -> np.array:
+    """
+    This is an array conversion wrapper that allows us to send arrays to both numpy and cupy.
+    :param lst: List to be converted.
+    :return: array
+    """
+    def _iter(_lst: list) -> list:  # TODO: We changed this, keep an eye on it.
+        """
+        This iterates down sublists.
+        :param _lst: Sublist.
+        """
+        if not isinstance(_lst, list):
+            _lst = list(lst)
+        for idx, item in enumerate(_lst):
+            if str(type(item)) == "<class 'cupy._core.core.ndarray'>" or str(type(item)) == "<class 'numpy.ndarray'>":
+                _lst[idx] = _iter(item)
+        return _lst
+    result = np.array(lst)
+    return result
+
+
+def percent_of(percent, whole, use_float=False):
+    """
+    Generic math method
+    """
+    result = np.divide(np.multiply(percent, whole), 100.0)
+    if not use_float:
+        result = int(result)
+    return result
+
+
+def percent_in(part, whole, use_float=False):
+    """
+    Generic math method
+    """
+    result = np.multiply(100.0, np.divide(part, whole))
+    if not use_float:
+        result = int(result)
+    return result
+
+
 def config(section: str) -> configparser:
     """
     Thius will grab our settings.
@@ -30,3 +87,4 @@ def config(section: str) -> configparser:
             value = cfg[setting]
         settings.update({setting: value})
     return settings
+
