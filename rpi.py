782afe2ef09ba66bdb99efafc695ccd3faa20fb0
Kevin Eales <kev0.0.0.255@gmail.com>
2021-09-29 16:52:58 -0400

    Added handler for wifi index error.

--- rpi.py
+++ rpi.py
@@ -13,20 +13,25 @@ from utils import system_command, percent_in, config
 
 
 settings = config('settings')
+skip = False
 
 
 def wifi_sig() -> int:
     """
     This will read our wifi signal strength.
     """
+    global skip
     cmd = 'iwconfig wlan0 | grep -i --color quality'
-    try:
-        info = system_command(cmd, shell=True)
-        percentages = info.split()[1].split('=')[1]
-        a, b = percentages.split('/')
-        strength = percent_in(int(a), int(b))
-    except (FileNotFoundError, IndexError) as err:
-        if settings['debug_startup']:
-            print(err)
-        strength = 0
+    strength = 0
+    if not skip:
+        try:
+            info = system_command(cmd, shell=True)
+            percentages = info.split()[1].split('=')[1]
+            a, b = percentages.split('/')
+            strength = percent_in(int(a), int(b))
+        except (FileNotFoundError, IndexError) as err:
+            if settings['debug_startup']:
+                print(err)
+            skip = True
+
     return strength
